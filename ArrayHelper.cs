57620815c1bb2a48d3ca09bbdc800ff36c6692df
faha-berdiev <madysoftware@gmail.com>
2020-04-17 00:55:02 +0300

    Добавленны новые функции в ArrayHelper

--- ArrayHelper.cs
+++ ArrayHelper.cs
@@ -1,14 +1,84 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
-namespace Program
+namespace Day_13
 {
-    public class ArrayHelper<T>
+    public static class ArrayHelper<T>
     {
-        public static T Pop<T>(ref T[] arr)
+        ///<summary>
+        ///Удаляет последний элемент массива и возвращает этот удалённый элемент.
+        ///</summary>
+        public static T Pop(ref T[] array)
         {
-            int endElement = arr.Length;
-            T results = arr[endElement];
-            return T;
+            if(array != null)
+            {
+                int endElement = array.Length - 1;
+                T results = array[endElement];
+                List<T> list = new List<T>();
+                list.AddRange(array);
+                list.RemoveAt(endElement);
+                array = list.ToArray();
+                return results;
+            }
+            else
+                return default(T);
+        }
+
+        ///<summary>
+        ///Добавляет элемент с конца массива и возвращает обновленную длину массива
+        ///</summary>
+        public static int Push(ref T[] array, T element)
+        {
+            if(array != null)
+            {
+                Array.Resize(ref array, array.Length + 1);
+                array[array.Length - 1] = element;
+            }
+            return array.Length;
+        }
+
+        ///<summary>
+        ///Удаляет первый элемент и возвращает этот удалённый элемент используя.
+        ///</summary>
+        public static T Shift(ref T[] array)
+        {
+            T results = default(T);
+            if(array != null)
+            {
+                results = array[0];
+                array = array.Skip(1).ToArray();
+            }
+            return results;
+        }
+
+        ///<summary>
+        ///Добавляет элемент в начало массива и возвращает обновленную длину массива
+        ///</summary>
+        public static int UnShift(ref T[] array, T element)
+        {
+            if(array != null)
+            {
+                T[] edited = array.Prepend(element).ToArray();
+                array = edited;
+            }
+            return array.Length;
+        }
+
+        ///<summary>
+        ///Возвращает новый массив, содержащий копию части исходного массива.
+        ///</summary>
+        public static T[] Slice(T[] array, int beginIndex, int endIndex)
+        {
+            T[] newArray = {};
+            return newArray;
+        }
+
+        public static void Output(T[] arr)
+        {
+            Array.ForEach(arr, it => {
+                Console.Write(it + ", ");
+            });
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -6,7 +6,65 @@ namespace Day_13
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string[] data = { }; //Массив с которым будет работать пользователь
+
+            string menu =  "\n1. Pop (Удаляет последний элемент)\n" +
+                           "2. Push (Добавляет элемент в конец списка)\n" +
+                           "3. Shift (Удаляет первый элемент)\n" +
+                           "4. UnShift (Добавляет элемент в начало списка)\n" +
+                           "5. Slice (Возвращает новый массив, содержащий копию части исходного массива)\n" +
+                           "6. Вывод списка\n" +
+                           "0. Выход\n";
+                           
+            Console.WriteLine("Здравстуйте! Вас приветствует редактор массива!");
+            Console.Write("Добавьте элементы в массив(через запятую, пример: Faridun,Firuz,Akmal,Rajab или введите '+' чтобы добавить в массив примерные элементы): ");
+
+            string cmd = string.Empty;
+            cmd = Console.ReadLine(); 
+
+            if(cmd == "+")
+                data = new string[4] { "Faridun", "Firuz", "Akmal", "Rajab" }; //Вводится примерные данные в массив
+            else
+                data = cmd.Split(','); //Пользователь вводит данные в массив
+
+            Console.WriteLine(menu);
+            while (cmd != "0")
+            {
+                cmd = Console.ReadLine();
+                if(cmd == "1") //Pop
+                {
+                    var deletedElement = ArrayHelper<string>.Pop(ref data);
+                    Console.WriteLine($"Удаленный элемент(метод Pop) - {deletedElement}");
+                }
+                else if(cmd == "2") //Push
+                {
+                    Console.Write("Введите элемент: ");
+                    string el = Console.ReadLine();
+                    int newSize = ArrayHelper<string>.Push(ref data, el);
+                    Console.WriteLine($"Новый размер(метод Push) - {newSize}");
+                }
+                else if(cmd == "3") //Shift
+                {
+                    var deletedElement = ArrayHelper<string>.Shift(ref data);
+                    Console.WriteLine($"Удаленный элемент(метод Shift) - {deletedElement}");
+                }
+                else if(cmd == "4") //UnShift
+                {
+                    Console.Write("Введите элемент: ");
+                    string el = Console.ReadLine();
+                    int newSize = ArrayHelper<string>.UnShift(ref data, el);
+                    Console.WriteLine($"Новый размер(метод UnShift) - {newSize}");
+                }
+                else if(cmd == "5") //Slice
+                {
+
+                }
+                else if(cmd == "6") //Output
+                {
+                    ArrayHelper<string>.Output(data);
+                }
+                Console.WriteLine(menu);
+            } 
         }
     }
 }
