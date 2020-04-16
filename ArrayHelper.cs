3eb58270af254f0ad80316742395502ca9d1e24e
faha-berdiev <madysoftware@gmail.com>
2020-04-16 23:13:26 +0300

    Создание класса ArrayHelper и функции Pop в нём

---
+++ ArrayHelper.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace Program
+{
+    public class ArrayHelper<T>
+    {
+        public static T Pop<T>(ref T[] arr)
+        {
+            int endElement = arr.Length;
+            T results = arr[endElement];
+            return T;
+        }
+    }
+}
